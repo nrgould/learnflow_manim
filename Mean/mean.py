0dfb5ca17c34b747150c4f6cf5d8be619879a701
nicholas <nrgould197@gmail.com>
2022-06-29 16:48:13 -0400

    second commit

--- Mean/mean.py
+++ Mean/mean.py
@@ -3,18 +3,22 @@ from manim import *
 config.pixel_width = 720
 config.pixel_height = 1280
 
+SMALL_TEXT = 36
+MEDIUM_TEXT = 44
+LARGE_TEXT = 72
+HUGE_TEXT = 200
+
+FONT = "Sora"
 
 class Mean(Scene):
     def construct(self):
         manimpango.list_fonts()
 
-        text1 = Text("Means", font_size=72)
-        text2 = Text("A mean (or average)", font_size=72, color=BLUE)
-        text3 = Text("is what you use to get", font_size=60)
-        text4 = Text("useful meaning out of", font_size=60)
-        text5 = Text("a bunch of numbers.", font_size=60)
-
-        intro_text = [text1, text2, text3, text4, text5]
+        text1 = Text("Means", font_size=LARGE_TEXT, font=FONT)
+        text2 = Text("A mean (or average)", font_size=LARGE_TEXT, color=BLUE, font=FONT)
+        text3 = Text("is what you use to get", font_size=MEDIUM_TEXT, font=FONT)
+        text4 = Text("useful meaning out of", font_size=MEDIUM_TEXT, font=FONT)
+        text5 = Text("a bunch of numbers.", font_size=MEDIUM_TEXT, font=FONT)
 
         text2.shift(UP * 2)
         text3.next_to(text2, DOWN, buff=0.5)
@@ -28,23 +32,22 @@ class Mean(Scene):
         self.play(Create(text3))
         self.play(Create(text4))
         self.play(Create(text5))
-        self.wait(3)
-        for text in intro_text:
-            self.play(FadeOut(text), run_time=0.5)
+        self.wait()
+        self.play(FadeOut(text1), FadeOut(text2), FadeOut(text3), FadeOut(text4), FadeOut(text5))
 
-        text6 = Text("A class has 5 students with test scores", font_size=60)
-        text7 = Text("Can't know how well the entire class did", font_size=60)
-        text8 = Text("just by looking at a single number ", font_size=60, slant=ITALIC, color=RED)
+        text6 = Text("A class has 5 students with test scores", font_size=MEDIUM_TEXT, font=FONT)
+        text7 = Text("You can't know how well the class did", font_size=MEDIUM_TEXT, font=FONT)
+        text8 = Text("just by looking at a single number ", font_size=MEDIUM_TEXT, slant=ITALIC, color=RED, font=FONT)
 
         text6.shift(UP * 3)
         text7.shift(UP * 3)
         text8.next_to(text7, DOWN, buff=0.5)
 
-        number1 = Text("66", font_size=36)
-        number2 = Text("80", font_size=36)
-        number3 = Text("85", font_size=36)
-        number4 = Text("92", font_size=36)
-        number5 = Text("76", font_size=36)
+        number1 = Text("66", font_size=SMALL_TEXT, font=FONT)
+        number2 = Text("80", font_size=SMALL_TEXT, font=FONT)
+        number3 = Text("85", font_size=SMALL_TEXT, font=FONT)
+        number4 = Text("92", font_size=SMALL_TEXT, font=FONT)
+        number5 = Text("76", font_size=SMALL_TEXT, font=FONT)
 
         numbers = [number1, number2, number3, number4, number5]
 
@@ -57,8 +60,11 @@ class Mean(Scene):
         self.play(Create(text6))
         self.wait()
 
-        for number in numbers:
-            self.play(Create(number), run_time=0.25)
+        # for number in numbers:
+        #     self.play(Create(number), run_time=0.25)
+
+        self.play(Create(number1), Create(number2), Create(number3), Create(number4), Create(number5), run_time=0.75)
+        self.wait(0.5)
 
         self.play(number1.animate.shift(UP * 6).shift(LEFT * 2.5).scale(2))
         self.play(number2.animate.next_to(number1, RIGHT, buff=2).scale(2))
@@ -68,24 +74,32 @@ class Mean(Scene):
 
         self.wait(1.5)
 
-        self.play(Transform(text6, text7))
+        self.play(text6.animate.shift(UP * 5))
+        self.play(Create(text7))
         self.wait()
         self.play(Create(text8))
 
-        box1 = SurroundingRectangle(number1, buff=0.2)
-        box2 = SurroundingRectangle(number2, buff=0.2)
-        box3 = SurroundingRectangle(number3, buff=0.2)
-        box4 = SurroundingRectangle(number4, buff=0.2)
-        box5 = SurroundingRectangle(number5, buff=0.2)
+        BOX_TRANSITION_TIME = 0.25
+        BOX_BUFFER = 0.2
+
+        box1 = SurroundingRectangle(number1, buff=BOX_BUFFER)
+        box2 = SurroundingRectangle(number2, buff=BOX_BUFFER)
+        box3 = SurroundingRectangle(number3, buff=BOX_BUFFER)
+        box4 = SurroundingRectangle(number4, buff=BOX_BUFFER)
+        box5 = SurroundingRectangle(number5, buff=BOX_BUFFER)
+
+        question_mark = Text("?", font_size=200, color=YELLOW)
+        question_mark.shift(DOWN * 3)
 
         self.play(Create(box1))
-        self.wait(0.5)
+        self.wait(BOX_TRANSITION_TIME)
         self.play(Transform(box1, box2))
-        self.wait(0.5)
+        self.wait(BOX_TRANSITION_TIME)
         self.play(Transform(box1, box3))
-        self.wait(0.5)
+        self.wait(BOX_TRANSITION_TIME)
         self.play(Transform(box1, box4))
-        self.wait(0.5)
+        self.wait(BOX_TRANSITION_TIME)
         self.play(Transform(box1, box5))
-
+        self.wait(BOX_TRANSITION_TIME)
+        self.play(Transform(box1, question_mark))
         self.wait()
